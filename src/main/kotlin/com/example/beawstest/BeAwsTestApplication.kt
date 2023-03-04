51e30c840cfa364ffd7049216a01d0e949b2130e
BangHyeonJun <davidbang@cj.net>
2023-03-04 22:33:15 +0900

    initial project

---
+++ src/main/kotlin/com/example/beawstest/BeAwsTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.beawstest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeAwsTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<BeAwsTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/beawstest/BeAwsTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.beawstest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeAwsTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
